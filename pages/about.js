3f066f9df5ac0db185dba12b55a8d0de820180bd
Bouamar Elouahhabi <iambouamar@gmail.com>
2018-02-16 21:12:43 +0100

    nextjs installed MaterialUI Theme configured

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+const About = ({title = "Hello from About!!"}) => (
+    <div>
+        <h2>{title}</h2>
+    </div>
+);
+
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+const Index = ({title = "Hello from Next.js!!"}) => (
+    <div>
+        <h2>{title}</h2>
+    </div>
+);
+
+
+export default Index;
